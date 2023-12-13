a4910a1151d2272f1f89ba68e53b84922ac8967f
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2023-12-13 11:03:29 +0100

    feat: add `dataHash` field (#13)
    Co-authored-by: MantisClone <david.hunt-mateo@mantisdata.solutions>

---
+++ src/hash-utils.ts
@@ -0,0 +1,64 @@
+import { ByteArray, crypto } from "@graphprotocol/graph-ts";
+import { Transaction } from "../generated/schema";
+
+// Returns the keccak256 hash of a strings
+export function computeHash(data: string): string {
+  return crypto.keccak256(ByteArray.fromUTF8(data)).toHexString();
+}
+
+// Returns a stringified conversion, as would be done by JSON.stringify in JS.
+export function serializeTransaction(tx: Transaction): string {
+  let data = tx.data;
+  if (data !== null) {
+    return serializeClearTxData(data);
+  }
+  let encryptedData = tx.encryptedData;
+  if (encryptedData !== null) {
+    return serializeEncryptedTxData(
+      encryptedData,
+      tx.encryptionMethod,
+      tx.publicKeys,
+      tx.encryptedKeys,
+    );
+  }
+  return "";
+}
+
+export function serializeClearTxData(data: string): string {
+  const cleanData = data.replaceAll('"', '\\"');
+  return `{"data":${addQuotes(cleanData)}}`.toLowerCase();
+}
+
+export function serializeEncryptedTxData(
+  data: string,
+  encryptionMethod: string | null,
+  publicKeys: string[] | null,
+  encryptedKeys: string[] | null,
+): string {
+  let str = `{"encryptedData":${addQuotes(data)}`;
+  if (encryptionMethod !== null) {
+    str += `,"encryptionMethod":${addQuotes(encryptionMethod)}`;
+  }
+  if (encryptedKeys && publicKeys) {
+    str += ',"keys":';
+    str += objectToJsonString(publicKeys, encryptedKeys);
+  }
+
+  str += "}";
+  return str.toLowerCase();
+}
+
+// given two arrays keys and values representing the object, returns a stringified JSON object, with sorted keys.
+export function objectToJsonString(keys: string[], values: string[]): string {
+  let result = new Array<string>(keys.length);
+
+  for (let i = 0; i < keys.length; ++i) {
+    result[i] = `${addQuotes(keys[i])}:${addQuotes(values[i])}`;
+  }
+
+  return "{" + result.sort().join(",") + "}";
+}
+
+function addQuotes(str: string): string {
+  return `"${str}"`;
+}
--- tests/transaction-data.test.ts
+++ tests/transaction-data.test.ts
@@ -6,13 +6,32 @@ describe("Transaction Data", () => {
     clearStore();
   });
 
-  test("should process transactions", () => {
-    processIpfs("transaction-data-string.json");
+  test("should process clear transactions", () => {
+    processIpfs("transaction-clear.json");
     assert.entityCount("Transaction", 1);
+    assert.fieldEquals("Transaction", "testIpfsHash-0", "hash", "testIpfsHash");
+    assert.fieldEquals(
+      "Transaction",
+      "testIpfsHash-0",
+      "dataHash",
+      "0x09c043a7cb846a048c2e665b832add72046fd9913e05f0c20e7f4f360ce62a8d",
+    );
+  });
+
+  test("should process encrypted transactions", () => {
+    processIpfs("transaction-encrypted.json");
+    assert.entityCount("Transaction", 1);
+    assert.fieldEquals("Transaction", "testIpfsHash-0", "hash", "testIpfsHash");
+    assert.fieldEquals(
+      "Transaction",
+      "testIpfsHash-0",
+      "dataHash",
+      "0x3e4bc69070baf9ed30ef58b87b454fd19841f18dd83f7dbe8b311bb32befc483",
+    );
   });
 
   test("should ignore wrong transaction data", () => {
-    processIpfs("transaction-data-object.json");
+    processIpfs("transaction-data-invalid-object.json");
     assert.entityCount("Transaction", 0);
   });
 });
