d2a9a1c2fbb40cff31341a748b3b386abe1f2298
rodrigopavezi <rodrigopavezi@gmail.com>
2024-11-11 20:54:42 -0300

    fix: hash function normalization for lit protocol cypher

--- src/hash-utils.ts
+++ src/hash-utils.ts
@@ -53,7 +53,7 @@ export function objectToJsonString(keys: string[], values: string[]): string {
   let result = new Array<string>(keys.length);
 
   for (let i = 0; i < keys.length; ++i) {
-    result[i] = `${addQuotes(keys[i])}:${addQuotes(values[i])}`;
+    result[i] = `${addQuotes(keys[i])}:${addQuotes(values[i].replaceAll('"', '\\\"'))}`;
   }
 
   return "{" + result.sort().join(",") + "}";
