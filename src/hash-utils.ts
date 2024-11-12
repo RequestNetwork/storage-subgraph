cae46efb802d8ead5ce83cc3e766d620989dc6bd
rodrigopavezi <rodrigopavezi@gmail.com>
2024-11-12 16:44:53 -0300

    fix: prettier removing backslash

--- src/hash-utils.ts
+++ src/hash-utils.ts
@@ -54,7 +54,8 @@ export function objectToJsonString(keys: string[], values: string[]): string {
 
   for (let i = 0; i < keys.length; ++i) {
     result[i] = `${addQuotes(keys[i])}:${addQuotes(
-      values[i].replaceAll('"', '\\"'),
+      // prettier-ignore
+      values[i].replaceAll('"', '\\\"'),
     )}`;
   }
 
